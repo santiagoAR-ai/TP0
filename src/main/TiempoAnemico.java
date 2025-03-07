17d469e34c8607f6cfa88c1812be30bbbaf678ab
santiagoAR-ai <santiagoabdala270@gmail.com>
2025-03-07 18:58:21 -0300

    first commit

---
+++ src/main/TiempoAnemico.java
@@ -0,0 +1,12 @@
+package main;
+
+public class TiempoAnemico {
+
+    public void mostrarLargo() {
+
+    }
+
+    public void mostrarCorto() {
+
+    }
+}
---
+++ src/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+import main.TiempoAnemico;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        TiempoAnemico tiempoAnemico = new TiempoAnemico();
+
+    }
+}
